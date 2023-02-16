3bbea537995c126e2ca25ede98a3cf47181e7f6f
dgzhuya <chenbiuxiu@gmail.com>
2023-02-17 00:38:25 +0800

    ✨ feat: 添加TS版本Promise

---
+++ promise_a_plus_test.spec.ts
@@ -0,0 +1,27 @@
+import PromiseAplusTests from 'promises-aplus-tests'
+// import MyPromise from './other_promise'
+import MyPromise from './promise_v1'
+
+const adapter = {
+	resolved(value) {
+		return new MyPromise((resolve, reject) => {
+			resolve(value)
+		})
+	},
+	rejected(reason) {
+		return new MyPromise((resolve, reject) => {
+			reject(reason)
+		})
+	},
+	deferred() {
+		let dfd: any = {}
+		dfd.promise = new MyPromise((resolve, reject) => {
+			dfd.resolve = resolve
+			dfd.reject = reject
+		})
+		return dfd
+	}
+}
+PromiseAplusTests(adapter, function (err) {
+	console.log(err)
+})
